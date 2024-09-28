cca5c439ea0fe1dd729d06815e4296b88e31ce45
sanyadixit <dixitsanya9@gmail.com>
2024-09-28 10:40:47 +0530

    first commit

---
+++ dead/src/main/java/com/example/dead/DeadApplication.java
@@ -0,0 +1,13 @@
+package com.example.dead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeadApplication.class, args);
+	}
+
+}
---
+++ dead/src/test/java/com/example/dead/DeadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
